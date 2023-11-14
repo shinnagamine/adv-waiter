8d73a7d533f586f1a34571cfca097d035edd1760
Shin N <78636399+shinnagamine@users.noreply.github.com>
2023-11-14 18:04:38 +0900

    Add files via upload

--- dist/adv-waiter.mjs
+++ dist/adv-waiter.mjs
@@ -9,42 +9,43 @@
  * @license     : Released under the MIT license.
  *                https://opensource.org/licenses/MIT
  */
-async function b(n, e) {
-  const i = n && isFinite(n) ? n : 100, t = e || (n && typeof n == "object" ? n : {}), o = t.until && typeof t.until == "function" ? t.until : null, u = t.while && typeof t.while == "function" ? t.while : null, l = t.callback && typeof t.callback == "function" ? t.callback : null, c = t.showDatetime;
+async function b(n, i) {
+  const e = n && isFinite(n) ? n : 100, t = i || (n && typeof n == "object" ? n : {}), o = t.until && typeof t.until == "function" ? t.until : null, u = t.while && typeof t.while == "function" ? t.while : null, c = t.callback && typeof t.callback == "function" ? t.callback : null, l = t.showDatetime;
   if (o || u) {
-    async function s(f, w, m) {
-      const y = t.timeout, r = t.onTimeout && typeof t.onTimeout == "function" ? t.onTimeout : null, T = Date.now();
-      if (w) {
+    async function s(f, w) {
+      const r = t.resultExistsIn;
+      if (r) {
+        const y = t.timeout, m = t.onTimeout && typeof t.onTimeout == "function" ? t.onTimeout : null, T = Date.now();
         let h;
-        for (; w.includes(h = await f()) === m; ) {
+        for (; r.includes(h = await f()) === w; ) {
           if (D(T, y))
-            return r && r(), null;
-          await a(i, c);
+            return m && m(), null;
+          await a(e, l);
         }
         return h;
       } else
-        for (; await f() === m; )
-          await a(i, c);
+        for (; await f() === w; )
+          await a(e, l);
     }
     if (o)
-      return s(o, t.untilResultExistsIn, !1);
+      return s(o, !1);
     if (u)
-      return s(u, t.whileResultExistsIn, !0);
+      return s(u, !0);
   } else
-    await a(i, c);
-  l && typeof l == "function" && l();
+    await a(e, l);
+  c && typeof c == "function" && c();
 }
 const d = {
   wait: b
 };
 window.AdvWaiter = d;
-function D(n, e) {
-  return e && Date.now() - n >= e;
+function D(n, i) {
+  return i && Date.now() - n >= i;
 }
-function a(n, e) {
-  return e && console.log(/* @__PURE__ */ new Date()), new Promise((i) => {
+function a(n, i) {
+  return i && console.log(/* @__PURE__ */ new Date()), new Promise((e) => {
     setTimeout(() => {
-      i();
+      e();
     }, n);
   });
 }
