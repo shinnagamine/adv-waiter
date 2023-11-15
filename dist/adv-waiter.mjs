c95216514f1afa47587487a8bf46bfe524673590
Shin N <78636399+shinnagamine@users.noreply.github.com>
2023-11-15 10:52:12 +0900

    Add files via upload

--- dist/adv-waiter.mjs
+++ dist/adv-waiter.mjs
@@ -4,14 +4,14 @@
  * @module      : adv-waiter
  * @description : This software is a JavaScript library that provides a couple of wait functions
  *                to simplify the source code and make it more readable.
- * @version     : 1.1.0
+ * @version     : 1.1.4
  * @author      : Shin Nagamine
  * @license     : Released under the MIT license.
  *                https://opensource.org/licenses/MIT
  */
-async function b(n, i) {
-  const e = n && isFinite(n) ? n : 100, t = i || (n && typeof n == "object" ? n : {}), o = t.until && typeof t.until == "function" ? t.until : null, u = t.while && typeof t.while == "function" ? t.while : null, c = t.callback && typeof t.callback == "function" ? t.callback : null, l = t.showDatetime;
-  if (o || u) {
+async function b(n, e) {
+  const i = n && isFinite(n) ? n : 100, t = e || (n && typeof n == "object" ? n : {}), o = t.while && typeof t.while == "function" ? t.while : null, a = t.until && typeof t.until == "function" ? t.until : null, u = t.callback && typeof t.callback == "function" ? t.callback : null, c = t.showDatetime;
+  if (o || a) {
     async function s(f, w) {
       const r = t.resultExistsIn;
       if (r) {
@@ -20,32 +20,29 @@ async function b(n, i) {
         for (; r.includes(h = await f()) === w; ) {
           if (D(T, y))
             return m && m(), null;
-          await a(e, l);
+          await l(i, c);
         }
         return h;
       } else
         for (; await f() === w; )
-          await a(e, l);
+          await l(i, c);
     }
-    if (o)
-      return s(o, !1);
-    if (u)
-      return s(u, !0);
+    return o ? s(o, !0) : s(a, !1);
   } else
-    await a(e, l);
-  c && typeof c == "function" && c();
+    await l(i, c);
+  u && typeof u == "function" && u();
 }
 const d = {
   wait: b
 };
 window.AdvWaiter = d;
-function D(n, i) {
-  return i && Date.now() - n >= i;
+function D(n, e) {
+  return e && Date.now() - n >= e;
 }
-function a(n, i) {
-  return i && console.log(/* @__PURE__ */ new Date()), new Promise((e) => {
+function l(n, e) {
+  return e && console.log(/* @__PURE__ */ new Date()), new Promise((i) => {
     setTimeout(() => {
-      e();
+      i();
     }, n);
   });
 }
